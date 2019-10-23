ad164cd469b0357670182cad93754474d981ecfd
Joeven Cerveza <cervezajoeven@gmail.com>
2019-10-23 12:09:20 +0800

    Announcement Banner Update

--- sics/application/views/version_1/general/dashboard/sms_index.php
+++ sics/application/views/version_1/general/dashboard/sms_index.php
@@ -43,19 +43,23 @@
                   <ol class="carousel-indicators">
                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                     <li data-target="#myCarousel" data-slide-to="1" class=""></li>
-                    <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+                    <li data-target="#myCarousel" data-slide-to="3" class=""></li>
                   </ol>
 
                   <!-- Wrapper for slides -->
                   <div class="carousel-inner">
                     <div class="item active">
-                      <img src="<?php echo $general_class->ben_resources('uploads/banners/image1.png'); ?>" alt="">
+                      <img src="<?php echo $general_class->ben_resources('uploads/banners/image_1.png'); ?>" alt="">
                     </div>
                     <div class="item">
-                      <img src="<?php echo $general_class->ben_resources('uploads/banners/image2.png'); ?>" alt="">
+                      <img src="<?php echo $general_class->ben_resources('uploads/banners/image_2.png'); ?>" alt="">
                     </div>
                     <div class="item">
-                      <img src="<?php echo $general_class->ben_resources('uploads/banners/image3.png'); ?>" alt="">
+                      <img src="<?php echo $general_class->ben_resources('uploads/banners/image_3.png'); ?>" alt="">
+                    </div>
+                    <div class="item">
+                      <img src="<?php echo $general_class->ben_resources('uploads/banners/image_4.png'); ?>" alt="">
                     </div>
                   </div>
 
--- sics/application/views/version_1/general/home/index.php
+++ sics/application/views/version_1/general/home/index.php
@@ -371,39 +371,43 @@
     <section id="about">
       <div class="container">
         <div class="col-lg-12 text-center">
-          <h3 class="section-heading text-uppercase">Announcements</h3>
-        </div>
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-          <!-- Indicators -->
-          <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1" class=""></li>
-            <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
-          </ol>
+                  <h3 class="section-heading text-uppercase">Announcements</h3>
+                </div>
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                  <!-- Indicators -->
+                  <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+                    <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+                    <li data-target="#myCarousel" data-slide-to="3" class=""></li>
+                  </ol>
 
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner">
-            <div class="item active">
-              <img src="<?php echo $general_class->ben_resources('uploads/banners/image1.png'); ?>" alt="">
-            </div>
-            <div class="item">
-              <img src="<?php echo $general_class->ben_resources('uploads/banners/image2.png'); ?>" alt="">
-            </div>
-            <div class="item">
-              <img src="<?php echo $general_class->ben_resources('uploads/banners/image3.png'); ?>" alt="">
-            </div>
-          </div>
+                  <!-- Wrapper for slides -->
+                  <div class="carousel-inner">
+                    <div class="item active">
+                      <img src="<?php echo $general_class->ben_resources('uploads/banners/image_1.png'); ?>" alt="">
+                    </div>
+                    <div class="item">
+                      <img src="<?php echo $general_class->ben_resources('uploads/banners/image_2.png'); ?>" alt="">
+                    </div>
+                    <div class="item">
+                      <img src="<?php echo $general_class->ben_resources('uploads/banners/image_3.png'); ?>" alt="">
+                    </div>
+                    <div class="item">
+                      <img src="<?php echo $general_class->ben_resources('uploads/banners/image_4.png'); ?>" alt="">
+                    </div>
+                  </div>
 
-          <!-- Left and right controls -->
-          <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
+                  <!-- Left and right controls -->
+                  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
       </div>
     </section>
 
