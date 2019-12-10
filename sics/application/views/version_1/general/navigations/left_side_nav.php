62fb60ac16b868d3caf6d5f7d96ac5fc9a614791
Joeven Cerveza <cervezajoeven@gmail.com>
2019-12-10 17:25:49 +0800

    Tawk to update

--- sics/application/views/version_1/general/navigations/left_side_nav.php
+++ sics/application/views/version_1/general/navigations/left_side_nav.php
@@ -361,17 +361,10 @@
     });
     
 </script>
-
+<?php if($general_class->session->userdata('account_type_id')==3||$general_class->session->userdata('account_type_id')==4): ?>
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-Tawk_API.onLoad = function(){
-    Tawk_API.setAttributes({
-        'name'  : 'Name',
-        'email' : 'email@email.com',
-        'hash'  : 'hash value'
-    }, function(error){});
-}
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
@@ -380,10 +373,6 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-
-
-
 </script>
-<?php if($general_class->session->userdata('account_type_id')==3||$general_class->session->userdata('account_type_id')==4): ?>
 <!--End of Tawk.to Script-->
 <?php endif; ?>
